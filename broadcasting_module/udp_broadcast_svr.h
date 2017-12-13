463102d2efcd77106fb1cd765538b3599f4de037
Udara Seneviratne <udara@localhost.localdomain>
2017-12-13 14:19:45 +0530

    integrated broadcasting svr modules

---
+++ broadcasting_module/udp_broadcast_svr.h
@@ -0,0 +1,141 @@
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <time.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#include "mkaddr.h"
+ 
+#ifndef TRUE
+#define TRUE 1
+#define FALSE 0
+#endif
+ 
+extern int mkaddr(
+				    void *addr,
+			    int *addrlen,
+			    char *str_addr,
+			    char *protocol);
+
+#define MAXQ 4
+
+void doBroadcast(char *msg);
+ 
+/*
+* This function reports the error and
+* exits back to the shell:
+*/
+static void
+displayError(const char *on_what) {
+  fputs(strerror(errno),stderr);
+  fputs(": ",stderr);
+  fputs(on_what,stderr);
+  fputc('\n',stderr);
+  exit(1);
+}
+
+void doBroadcast(char *msg) {
+  int z;      /* Status return code */
+  int s;      /* Socket */
+  struct sockaddr_in adr_srvr;/* AF_INET */
+  int len_srvr;               /* length */
+  struct sockaddr_in adr_bc;  /* AF_INET */
+  int len_bc;                 /* length */
+  static int so_broadcast = TRUE;
+  static char
+  *sv_addr = "127.0.0:*",
+  *bc_addr = "127.255.255.2 9097";
+ 
+ /*
+  * Form the server address:
+  */
+  len_srvr = sizeof adr_srvr;
+
+  z = mkaddr(
+ 		  &adr_srvr,  /* Returned address */
+		  &len_srvr,  /* Returned length */
+		  sv_addr,    /* Input string addr */
+		  "udp");     /* UDP protocol */
+  	adr_srvr.sin_family = AF_INET;
+  	adr_srvr.sin_port = (in_port_t)htons(9097);
+		adr_srvr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+  if ( z == -1 )
+	displayError("Bad server address");
+ 
+ /*
+  * Form the broadcast address:
+  */
+  len_bc = sizeof adr_bc;
+ 
+  z = mkaddr(
+	  &adr_bc, /* Returned address */
+	  &len_bc, /* Returned length */
+	  bc_addr, /* Input string addr */
+	  "udp"); /* UDP protocol */
+  	adr_bc.sin_addr.s_addr = htonl(INADDR_BROADCAST);
+  	adr_bc.sin_port = (in_port_t)htons(9098);
+ 
+  if ( z == -1 )
+	displayError("Bad broadcast address");
+ 
+ /*
+  * Create a UDP socket to use:
+  */
+  s = socket(AF_INET,SOCK_DGRAM,0);
+  if ( s == -1 )
+	displayError("socket()");
+ 
+ /*
+  * Allow broadcasts:
+  */
+  z = setsockopt(s,
+				 SOL_SOCKET,
+				 SO_BROADCAST,
+				 &so_broadcast,
+				 sizeof so_broadcast);
+ 
+  if ( z == -1 )
+	displayError("setsockopt(SO_BROADCAST)");
+
+ /*
+  * Bind an address to our socket, so that
+  * client programs can listen to this
+  * server:
+  */
+  z = bind(s,
+		  (struct sockaddr *)&adr_srvr,
+		  len_srvr);
+ 
+  if ( z == -1 )
+	displayError("bind()");
+ 
+  for (;;) {
+
+	 /*
+	  * Broadcast the updated info:
+	  */
+
+	  // char * msg = "Hi I am Udara";
+  	char buffer[256];
+  	printf("Please enter the message :\n");
+	bzero(buffer, 256);
+	fgets(buffer,255,stdin);
+
+	  z = sendto(s,
+				 buffer,
+				 strlen(buffer),
+				 0,
+				(struct sockaddr *)&adr_bc,
+				len_bc); 
+	 
+	  if ( z == -1 )
+		displayError("sendto()");
+
+	  sleep(4);
+  }
+ 
+}
--- raspy.c
+++ raspy.c
@@ -9,6 +9,7 @@
 #include "sensor_modules/ultra_sound.h"
 #include "sensor_modules/reverse_sensor.h"
 #include "sensor_modules/humidity.h"
+#include "broadcasting_module/udp_broadcast_svr.h"
 
 int setupWiringPi();
 void showSampleMenu();
@@ -33,7 +34,8 @@ void showSampleMenu(){
 	printf("3 - Ring Buzzer [Anode PIN 8] \n");
 	printf("4 - Ultra sound distance detector [VCC PIN 5V, Trig PIN 7, Echo PIN 0] \n");
 	printf("5 - Reverse Sensor [VCC PIN 5V, Trig PIN 7, Echo PIN 0, Buzzor Anode 8] \n");
-	printf("6 - Humidity Sensor [VCC PIN 5V, Trig PIN 7] \n\n");
+	printf("6 - Humidity Sensor [VCC PIN 5V, Trig PIN 7] \n");
+	printf("7 - Start signal broadcasting \n\n");
 
 	printf("Enter the choice index :");
 	scanf("%d", &choice);
@@ -62,6 +64,9 @@ void triggureAction(int *choice){
 		break;
 		case 6:
 			readHumidity();
+		break;
+		case 7:
+			doBroadcast("Hello World");
 		break;
 	}
 }
